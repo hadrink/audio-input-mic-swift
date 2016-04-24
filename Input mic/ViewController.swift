10a126acbd61e9a24ffd2861e7b458b78a4d8fe3
RomainRui <romain.rui10@gmail.com>
2016-04-24 10:24:15 +0200

    Initial Commit

---
+++ Input mic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Input mic
+//
+//  Created by Rplay on 24/04/16.
+//  Copyright © 2016 rplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
